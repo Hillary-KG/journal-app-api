e6ee2fa6a339993481a0d3b979e948451ba41654
hillary <gachukihillary@gmail.com>
2024-07-04 17:16:27 +0300

    Application Update add auth blueprint add logging add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import os
+import logging
 from datetime import datetime
+from logging.handlers import RotatingFileHandler
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -8,12 +10,15 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from flask.logging import default_handler
 
 mail = Mail()
 jwt_manager = JWTManager()
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
+cors = CORS()
+
 
 def create_app(config_class="config.DevelopmentConfig"):
     """this function creates and configure the flask app"""
@@ -21,14 +26,18 @@ def create_app(config_class="config.DevelopmentConfig"):
     app.config.from_object(config_class)
     initialize_extensions(app)
     register_blueprints(app)
-    
+    configure_logging(app)
+
     return app
-    
-    
-    
-    
+
+
 def register_blueprints(app):
-    pass
+    from app.users import users_bp
+
+    app.register_blueprint(users_bp, url_prefix="/api/auth")
+
+    return
+
 
 def initialize_extensions(app):
     mail.init_app(app)
@@ -36,9 +45,33 @@ def initialize_extensions(app):
     migrate.init_app(app, db)
     jwt_manager.init_app(app)
     login_manager.init_app(app)
+    cors.init_app(app)
 
-def configure_login(app):
-    pass
+
+def configure_logging(app):
+    if app.config["LOG_TO_STDOUT"]:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+        app.logger.removeHandler(default_handler)
+        file_handler = RotatingFileHandler(
+            "logs/app.log", maxBytes=16384, backupCount=3
+        )
+
+        file_handler.setLevel(logging.INFO)
+        logs_formatter = logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(module)s %(filename)s: %(lineno)d]"
+        )
+        file_handler.setFormatter(logs_formatter)
+
+        app.logger.handlers.clear()
+        app.logger.addHandler(file_handler)
+
+        return
+
 
 @jwt_manager.user_identity_loader
 def user_identity_lookup(user):
